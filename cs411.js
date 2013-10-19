6b233d47cd6b791326fe574d10e0b1ab58bb6b8e
kylejao <kylejao@gmail.com>
2013-10-19 13:48:32 -0500

    1st step finished

--- cs411.js
+++ cs411.js
@@ -1,18 +1,19 @@
 (function() {
-  var self = this;
   $.ajax({
     url: './post.php?request=category',
     dataType: 'json',
     success: function(result) {
       var cat = $("#category");
-      for (var i=0;i<result;i++) {
-      var option=document.createElement("option");
-      option.text="Kiwi";
-      try {
-        cat.append(option);
-      } catch (e) {
-        alert(cat.size);
+      for (var i=0; i<result.length; i++) {
+        var option=document.createElement("option");
+        option.text=result[i];
+        try {
+          cat.append(option);
+        } catch (e) {
+          alert(cat.size);
+        }
       }
-    }
+    },
+    error: function() {alert("..");}
   });
 })();
--- post.php
+++ post.php
@@ -2,20 +2,18 @@
 $con=mysqli_connect("localhost","root","cs411gogo","CS411Test");
 // Check connection
 //$firstName = $_POST['FirstName'];
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-if($_GET['request']=='categories'){
+if ($_GET['request']=='category') {
   echo getCategories($con);
 }
-function getCategories($con)
-{
+function getCategories($con) {
   $sql = "SELECT name FROM categories";
   $result = mysqli_query($con, $sql);
   $ret = array();
-  while($row = mysqli_fetch_array($result)){
+  while ($row = mysqli_fetch_array($result)){
     array_push($ret, $row['name']);
   }
   header('Content-type: application/json');
