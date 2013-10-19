5807a7c49d6fb6da5982ec05a2f02ca6baa758cb
kylejao <kylejao@gmail.com>
2013-10-19 13:34:38 -0500

    added a script for git log

---
+++ git_config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config --global color.ui auto
+git config --global alias.lg "log --graph --format=format:'%Cblue%h%Creset - %s %Cgreen%d%Creset %C(dim white)%an%Creset' --abbrev-commit"
