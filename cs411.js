e7febbc98ab5731de27001871a04e95d5c107d3f
kylejao <kylejao@gmail.com>
2013-10-20 17:36:33 -0500

    refactored js and kind of added search and post functionalities

--- cs411.js
+++ cs411.js
@@ -1,8 +1,8 @@
-(function() {
-  $.ajax({
-    url: './post.php?request=category',
-    dataType: 'json',
-    success: function(result) {
+newFunc(function() {
+  ajaxCall(
+    './post.php?request=category',
+    null,
+    function(result) {
       var cat = $("#category");
       for (var i=0; i<result.length; i++) {
         var option=document.createElement("option");
@@ -13,7 +13,55 @@
           alert(cat.size);
         }
       }
-    },
-    error: function() {alert("..");}
+    }
+  );
+});
+
+newClickHandler("post", function() {
+  ajaxCall(
+    './post.php',
+    { QuestionDesc: $("question_desc").val() },
+    null,
+    null,
+    'post'
+  );
+});
+
+newClickHandler("search", function() {
+  ajaxCall(
+    './post.php',
+    { QuestionDesc: $("question_desc").val() },
+    null,
+    null,
+    'post'
+  );
+});
+
+newClickHandler("button1", function() {
+  alert("click me");
+});
+
+function newFunc(func) {
+  $(document).ready(func);
+}
+
+function newClickHandler(id, callback) {
+  newFunc(function() {
+    id = "#"+id;
+    $(id).click(callback);
   });
-})();
+}
+
+function ajaxCall(url, data, successCallback, errorCallback, type) {
+  if (typeof type === "undefined") {
+    type = 'get';
+  }
+  $.ajax({
+    url: url,
+    type: type,
+    dataType: 'json',
+    data: data,
+    success: successCallback,
+    error: errorCallback
+  });
+}
