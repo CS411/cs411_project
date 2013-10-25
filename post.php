67df67b59406e50b7ecfd8643ad389cd4b9191e1
Caroline Li <carolineli.tw@gmail.com>
2013-10-24 21:52:11 -0500

    Update post.php

--- post.php
+++ post.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-if ($_GET['request']=='category') {
+if ($_GET['request']=='categories') {
   getCategories($con);
 }
 
