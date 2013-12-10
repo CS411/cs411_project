ecb8df3d05d480805bb8097bcd58fcd4dbb9fd82
kylejao <kylejao@gmail.com>
2013-12-09 23:09:48 -0600

    minor

--- cky.js
+++ cky.js
@@ -100,15 +100,16 @@ function new_link(content, id) {
   }
   return link;
 }
-function _handle_open_code_area_button_click(){
-  if(open_code_flag == 1){
+
+function _handle_open_code_area_button_click() {
+  if (open_code_flag == 1) {
     $("#editor").show();
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.getSession().setMode("ace/mode/c_cpp");
     $('#show_or_hide_code_area_button').text("Hide Code Editor")
     open_code_flag = 0;
-  }else{
+  } else {
     $("#editor").hide();
     $('#show_or_hide_code_area_button').text("Open Code Editor")
     open_code_flag = 1;
